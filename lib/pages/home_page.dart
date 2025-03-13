f2ded7419368b6c11ac1924bb50728d0376abdfe
Samay <samay87430@gmail.com>
2025-03-13 13:16:37 +0530

    Day 12 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,30 +1,60 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Codepur";
+
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+    .map<Item>((item)=>Item.fromMap(item))
+    .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(100, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
+        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)?ListView.builder(
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
-              item: dummyList[index],
+              item: CatalogModel.items[index],
             );
           },
-        ),
+        ):Center(child: CircularProgressIndicator(),),
       ),
       drawer: MyDrawer(),
     );
   }
-}
+} 
